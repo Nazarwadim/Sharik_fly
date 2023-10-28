222874cd3a784cd340be6622cbbdcc172db27aa2
nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2023-10-28 18:14:17 +0300

    fix wind generation on android Wind_generation.gd

--- scenes/Level/Wind/Wind_generator.gd
+++ scenes/Level/Wind/Wind_generator.gd
@@ -23,7 +23,7 @@ func generate_wind(ball:Ball):
 		await get_tree().create_timer(randf_range(0.01, 1)).timeout
 
 func _create_wind(ball_position:Vector2, wind_setup_position:Vector2, wind_trail_speed:float, wind_trail_length:float) -> WindGust:
-	var wind = wind_scene.instantiate(PackedScene.GEN_EDIT_STATE_MAIN) as WindGust
+	var wind = wind_scene.instantiate() as WindGust
 	wind.set_wind_path_position(ball_position + wind_setup_position)
 	wind.trail_speed = wind_trail_speed
 	wind.trail_length = wind_trail_length
--- scenes/Level/level.gd
+++ scenes/Level/level.gd
@@ -15,27 +15,20 @@ func stop_generation(_balL):
 	$Kaktuses_generator/Timer.stop()
 	$Cloud_Paralax/Timer.stop()
 	
+func start_generation():
+	$Kaktuses_generator/Timer.start(1)
+	$Cloud_Paralax/Timer.start(1)
+	
+	
 func on_generate_timer_timeout() -> void:
 	$Kaktuses_generator/Timer.wait_time = kaktuses_generation_frequency_distance / player.phisics.speed
 	$Kaktuses_generator.generate_kaktus(player)
 	on_kaktus_generate.emit()
 
-
-
 func _on_cloud_timer_timeout() -> void:
 	$Cloud_Paralax/Timer.wait_time = cloud_generation_frequency_dustance / player.phisics.speed
 	$Cloud_Paralax.generate_cloud(player)
 	on_cloud_generate.emit()
-
-
-
-func start_generation():
-	$Kaktuses_generator/Timer.start(1)
-	$Cloud_Paralax/Timer.start(1)
-
-func start_wind_generation(wait_time):
-	$Wind_generator/Timer.start(wait_time)
-
 
 func _on_generate_wind_timer_timeout():
 	$Wind_generator/Timer.wait_time = randf_range(2,5)
